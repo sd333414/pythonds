73643728946a19aa042e7be1fbdc88cc3205119b
SaiJeevan <sd333414@ohio.edu>
2019-09-05 13:50:34 -0500

    Added Average marks of the students in the dictionary with running average

---
+++ avgdict.py
@@ -0,0 +1,16 @@
+ia = [['sai',100],['jeevan',200],['sai',200],['jeevan',700],['sai',900],['jeevan',2000],['sai',900]]
+b = {}
+c = {}
+for i in a:
+    name=i[0]
+    score=i[1]
+    if name in b:
+
+        b[name]=(b[name]+score)
+        c[name]=c[name]+1
+
+    else:
+        b[name]=score
+        c[name]=1
+print(b)
+print(c)
